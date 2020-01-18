f0abe2f4977662c1632221a2ec71b363613be93a
Rick Hallett <rickhallett@live.co.uk>
2020-01-18 12:59:46 +0000

    initial migration setup

---
+++ Migrations/20200118114514_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HypertropeCore.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sets",
+                columns: table => new
+                {
+                    SetId = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Exercise = table.Column<int>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    Reps = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sets", x => x.SetId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Metrics",
+                columns: table => new
+                {
+                    MetricId = table.Column<Guid>(nullable: false),
+                    Volume = table.Column<int>(nullable: false),
+                    OneRm = table.Column<int>(nullable: false),
+                    SetId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Metrics", x => x.MetricId);
+                    table.ForeignKey(
+                        name: "FK_Metrics_Sets_SetId",
+                        column: x => x.SetId,
+                        principalTable: "Sets",
+                        principalColumn: "SetId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Metrics_SetId",
+                table: "Metrics",
+                column: "SetId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Metrics");
+
+            migrationBuilder.DropTable(
+                name: "Sets");
+        }
+    }
+}
