8e756da4bde960dd4946397840b300cf745b3947
Rick Hallett <rickhallett@live.co.uk>
2020-01-20 11:11:42 +0000

    implemented installer pattern

---
+++ Installers/MvcInstaller.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HypertropeCore.Extensions;
+using HypertropeCore.Options;
+using HypertropeCore.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HypertropeCore.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(nameof(jwtSettings), jwtSettings);
+            services.AddSingleton(jwtSettings);
+            
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+
+            services.AddAuthentication();
+            services.ConfigureIdentity();
+            
+            services.AddControllers();
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddScoped<IWorkoutService, WorkoutService>();
+        }
+    }
+}
---
+++ Options/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace HypertropeCore.Options
+{
+    public class JwtSettings
+    {
+        public string ValidIssuer { get; set; }
+        public string ValidAudience { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
