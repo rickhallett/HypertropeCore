fa18e93f7771e6ab71580eac1b31f9f739d01ce4
richardhallett <rickhallett@live.co.uk>
2020-02-16 10:15:52 +0000

    MeditationLog.cs as new tracking concept

---
+++ Domain/MeditationLog.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HypertropeCore.Domain
+{
+    public class MeditationLog
+    {
+        [Key]
+        [Column("MeditationLogId")]
+        public Guid MeditationLogId { get; set; }
+        
+        [Required(ErrorMessage = "Notes are a required field. This is the entire point.")]
+        public string Notes { get; set; }
+        
+        [Required(ErrorMessage = "Anxiety is a required field, min = 0, max = 10")]
+        [Range(0, 10)]
+        public int Silence { get; set; }
+        
+        [Required(ErrorMessage = "Willingness is a required field, min = 0, max = 10")]
+        [Range(0, 10)]
+        public int Willingness { get; set; }
+    }
+}
